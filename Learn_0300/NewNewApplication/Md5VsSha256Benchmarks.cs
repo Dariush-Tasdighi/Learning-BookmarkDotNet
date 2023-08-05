fc9276bc040a31be8a68c80333e0e36235edde44
Mr. Dariush Tasdighi <DariushT@GMail.com>
2023-08-05 18:50:20 +0330

    Version 1.0.2

---
+++ Learn_0300/NewNewApplication/Md5VsSha256Benchmarks.cs
@@ -0,0 +1,40 @@
+﻿namespace Application;
+
+public class Md5VsSha256Benchmarks
+{
+	private System.Security.Cryptography.MD5
+		md5 = System.Security.Cryptography.MD5.Create();
+
+	private System.Security.Cryptography.SHA256
+		sha256 = System.Security.Cryptography.SHA256.Create();
+
+	private byte[] data;
+
+	[BenchmarkDotNet.Attributes.Params(1_000, 10_000)]
+	public int N;
+
+	[BenchmarkDotNet.Attributes.GlobalSetup]
+	public void Setup()
+	{
+		data = new byte[N];
+		new System.Random(42).NextBytes(data);
+	}
+
+	[BenchmarkDotNet.Attributes.Benchmark]
+	public byte[] Md5()
+	{
+		var result =
+			md5.ComputeHash(buffer: data);
+
+		return result;
+	}
+
+	[BenchmarkDotNet.Attributes.Benchmark]
+	public byte[] Sha256()
+	{
+		var result =
+			sha256.ComputeHash(buffer: data);
+
+		return result;
+	}
+}
---
+++ Learn_0300/NewNewApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Application;
+
+internal static class Program : object
+{
+	static Program()
+	{
+	}
+
+	private static void Main()
+	{
+		BenchmarkDotNet.Running.BenchmarkRunner.Run
+			(assembly: typeof(Program).Assembly);
+	}
+}
